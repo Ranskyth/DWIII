ac89a38d9a8025abdc0dbfb323418fe688b0aaaf
Ranskyth <ga.lima2205@gmail.com>
2026-08-25 11:13:02 -0300

    chore: deixei mais clean

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const http = require("http")
-const fs = require("fs")
-const app = require("./src/rotas.js")
+const http = require("http");
+const app = require("./src/rotas.js");
+const { PORT } = require("./config.js");
+let server = http.createServer(app.rotas);
 
-
-let server = http.createServer(app.rotas)
-
-server.listen(3333)
-console.log("server on")
+server.listen(PORT);
+console.log(`server on - http://localhost:${PORT}`);
